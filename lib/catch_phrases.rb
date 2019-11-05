2b9959c6e59c90791b6f7f3ffbca1cc148424639
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-05 23:42:32 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,3 +1,5 @@
+#lib/catch_phrases.rb
+
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
     phrase = "It's-a me, Mario!"
@@ -15,6 +17,13 @@ def link do
     phrase="It's Dangerous To Go Alone! Take This"
     expect{link}.to output("It's Dangerous to Go Alone!Take This\n").to.stdout
     
+    describe "any_phrase" do
+  it "takes in an argument and puts out the catch phrase" do
+    phrase = "Do A Barrel Roll!"
+    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+  end
+  
+end
 rspec spec/catch_phrases_spec.rb
 spec/catch_phrase_spec.rb
 
